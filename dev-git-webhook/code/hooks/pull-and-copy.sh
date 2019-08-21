1e549290bd494609272dc24842edcc43fcd73030
Johannes Trame <jt@metaphacts.com>
2019-08-21 10:00:15 +0000

    Update webhook container to be compatible with 3.x /runtime-data folder

--- dev-git-webhook/code/hooks/pull-and-copy.sh
+++ dev-git-webhook/code/hooks/pull-and-copy.sh
@@ -4,8 +4,8 @@ GIT_DIRECTORY="/git"
 if [ -d "$GIT_DIRECTORY" ]; then
 	cd ${GIT_DIRECTORY}/*
 	git pull origin
-	cp -R data/* /data/
+#	cp -R data/* /data/
 	cp -R ./* /apps/${WEBHOOK_APP_DIRECTORY}
-	chown -R 100:101 /data
+#	chown -R 100:101 /data
 fi
 
--- dev-git-webhook/code/init.sh
+++ dev-git-webhook/code/init.sh
@@ -7,7 +7,9 @@ if [ ! -d "$GIT_DIRECTORY" ]; then
 	cd ${GIT_DIRECTORY} 
 	git clone -b ${WEBHOOK_BRANCH_LIST} ${WEBHOOK_GIT_REPOSITORY}
 	cd ./*
-	cp -R data/* /data/
+	#cp -R data/* /data/
+	mkdir /apps/${WEBHOOK_APP_DIRECTORY}
+	chown -R 100:101 /apps/${WEBHOOK_APP_DIRECTORY}
 	cp -R ./* /apps/${WEBHOOK_APP_DIRECTORY}
-	chown -R 100:101 /data
+	#chown -R 100:101 /data
 fi
