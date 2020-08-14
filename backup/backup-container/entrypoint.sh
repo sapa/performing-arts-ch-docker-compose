9c88343f9839f222926ac2d52a25f0531a5caa20
Dominik Hach-Schwartz <dominik.hach-schwartz@netlution.de>
2020-08-14 11:16:12 +0200

    fix backup container errors
    remove failing find command; reformat script and suppress s3 output.

--- backup/backup-container/entrypoint.sh
+++ backup/backup-container/entrypoint.sh
@@ -14,20 +14,15 @@ if [ -z "${S3_BUCKET_URL}" ]; then
 fi
 if [ -n "${S3_BUCKET_URL}" ]; then
 	echo "Uploading backup archive ${BACKUP_FILE_PATH} to S3 ${S3_BUCKET_URL}"
-	aws s3 cp "${BACKUP_FILE_PATH}" "${S3_BUCKET_URL}" --endpoint-url "${ENDPOINT_URL}"
+	aws s3 cp "${BACKUP_FILE_PATH}" "${S3_BUCKET_URL}" --quiet --endpoint-url "${ENDPOINT_URL}"
 
 	echo "Uploading runtime backup archive ${BACKUP_RUNTIME_FILE_PATH} to S3 ${S3_BUCKET_URL}"
-        aws s3 cp "${BACKUP_RUNTIME_FILE_PATH}" "${S3_BUCKET_URL}" --endpoint-url "${ENDPOINT_URL}"
-
+	aws s3 cp "${BACKUP_RUNTIME_FILE_PATH}" "${S3_BUCKET_URL}" --quiet --endpoint-url "${ENDPOINT_URL}"
 fi
 
 if [ -n "${KEEP_BACKUP_DAYS}" ]; then
 	echo "Removing old backups. Only keeping backups created in the last ${KEEP_BACKUP_DAYS} days."
 	find "${BACKUP_BASE_PATH}" -mtime "+${KEEP_BACKUP_DAYS}" -type f -delete;
-
-	echo "Removing old runtime backups. Only keeping backups created in the last ${KEEP_BACKUP_DAYS} days."
-        find "${BACKUP_RUNTIME_BASE_PATH}" -mtime "+${KEEP_BACKUP_DAYS}" -type f -delete;
-
 else
 	echo "KEEP_BACKUP_DAYS was not set. Not removing any old backups."
 fi
