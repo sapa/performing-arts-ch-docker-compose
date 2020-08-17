b2b5bc1015b590d5b09c17578f69c3bdb70ef268
Hach-Schwartz <Dominik.Hach-Schwartz@netlution.de>
2020-08-17 09:15:25 +0200

    rework of backup; doesn't need to stop containers anymore

--- backup/callToBackup.sh
+++ backup/callToBackup.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
+
+set -e
+
 # This file shall be executed by a cronjon to trigger the backup process
 PATH_TO_SCRIPT=/home/ubuntu/performing-arts-ch-docker-compose/backup/
 
 ### ADD THE NAME OF ANY ENVIRONMENT TO BE BACKED UP TO THIS ARRAY
 declare -a environments=(
-   # "dev"
-    "prod"
+    "dev"
+   # "prod"
 )
 
-#### START CODE, DO NOT EDIT ####
+backup_date=$(date +%Y-%m-%d_%H-%M)
 
+
+#### START CODE, DO NOT EDIT ####
 # backup function
 # takes the name of the environment as a parameter
 function backup {
-    echo "[$(date "+%Y-%m-%d")] Starting backup for ${environment}"
+    echo "[${backup_date}] Starting backup for ${environment}"
+
+    echo "-> starting online backup of Blazegraph"
+    blazegraph_container_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${environment}-blazegraph)
+    curl --data-urlencode "file=/blazegraph-data/blazegraph.jnl.backup" --data-urlencode "compress=true" http://${blazegraph_container_ip}:8080/blazegraph/backup
 
     echo "-> Navigating into working directory"
     currentDir="$(pwd)"
@@ -21,22 +30,14 @@ function backup {
 
     cd ../metaphactory-blazegraph/"${environment}"/ || return
 
-    echo "-> Shutting down metaphacts-platform containers"
-    docker-compose down
-
     echo "-> Starting cron-backup container"
     docker-compose -f ../../backup/docker-compose.yml up
 
-    echo "Starting up metaphacts-platform containers again"
-    docker-compose up -d
-
     cd "${currentDir}" || return
 
-    echo "[$(date "+%Y-%m-%d")] Finished backup for ${environment}"
+    echo "[${backup_date}] Finished backup for ${environment}"
 }
 
-
-for environment in "${environments[@]}"
-do
-  backup "${environment}"
+for environment in "${environments[@]}"; do
+    backup "${environment}"
 done
